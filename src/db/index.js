955b1ecf9212abf2e222cefb60080a4334221ba5
shubhamsonawane12 <127851148+shubhamsonawane12@users.noreply.github.com>
2024-01-17 03:13:51 +0530

    mongo db connect

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+const connectDB = async ()=>{
+    try{
+
+        await mongoose.connect(process.env.MONGODB_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            dbName:DB_NAME,
+        });
+console.log(`\n MongoDB connect !!DB HOST: ${mongoose.connection.host}`);
+    }catch(error){
+        console.error("MONGODB connection error",error);
+        process.exit(1);
+    }
+}
+export default connectDB
